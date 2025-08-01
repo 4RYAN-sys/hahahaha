5ef7c3e399f5b00c8bc9d176d1e6d77e0da1b53f
4RYAN-sys <susantoaryan488@gmail.com>
2025-08-01 09:51:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showContact() {
+    const info = document.getElementById("contact-info");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+}
