a02053efcabc0691fc9cdf29073694e8b9b4518d
4RYAN-sys <susantoaryan488@gmail.com>
2025-08-05 17:49:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Ngetik teks
-let i = 0, txt = "Hi! Saya Aryan Pratama S.👋", speed = 80;
+
+let i = 0, txt = "Hi! Selamat Datang.👋", speed = 80;
 (function type() {
   if (i < txt.length) {
     document.getElementById("typewriter").textContent += txt[i++];
@@ -7,15 +7,14 @@ let i = 0, txt = "Hi! Saya Aryan Pratama S.👋", speed = 80;
   }
 })();
 
-// Dark/Light mode toggle
 const toggleBtn = document.getElementById("toggle-mode");
 toggleBtn.onclick = () => {
   document.body.classList.toggle("light");
   toggleBtn.textContent = document.body.classList.contains("light") ? "🌑" : "🌙";
 };
 
-// Klik kartu -> alert
 document.querySelectorAll(".card").forEach(card =>
   card.onclick = () => alert(` ${card.querySelector("h3").textContent}`)
 );
+
 
